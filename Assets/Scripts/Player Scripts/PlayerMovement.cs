b182a48820486f08f4c1ad35cd7ed34bab7284a2
Masih Bahmani <bahmani.masih@gmail.com>
2022-01-04 17:24:01 +0330

    Implement sprint and crouch in PlayerMovement

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -6,14 +6,24 @@ using static Tags;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Transform look_Root;
     CharacterController characterController;
     Vector3 moveDirection;
-    public float speed = 5f, jumpForce = 10f;
-    private float verticalSpeed, gravity =  20f;
+
+    public float speed, normalSpeed = 5f, sprintSpeed = 10f, crouchSpeed = 2f
+        , jumpForce = 10f;
+    private float verticalSpeed, gravity = 20f;
+
+    private float stand_Height = 0f;
+    private float crouch_Height = -0.6f;
+
+    private bool isCrouching;
 
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        look_Root = transform.GetChild(0);
+        speed = normalSpeed;
     }
 
     // Update is called once per frame
@@ -24,13 +34,51 @@ public class PlayerMovement : MonoBehaviour
 
     void PlayerMove()
     {
+        Sprint();
+        Crouch();
+
         moveDirection = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f, Input.GetAxis(Axis.VERTICAL));
         moveDirection = transform.TransformDirection(moveDirection);
         moveDirection *= speed * Time.deltaTime;
 
         ApplyGravity();
-     
+
         characterController.Move(moveDirection);
+    }
+
+    private void Sprint()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isCrouching = false;
+            look_Root.localPosition = new Vector3(0f, stand_Height, 0f);
+            speed = sprintSpeed;
+        }
+
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+            speed = normalSpeed;
+    }
+
+
+    void Crouch()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (isCrouching)
+            {
+                look_Root.localPosition = new Vector3(0f, stand_Height, 0f);
+                speed = normalSpeed;
+
+                isCrouching = false;
+            }
+            else
+            {
+                look_Root.localPosition = new Vector3(0f, crouch_Height, 0f);
+                speed = crouchSpeed;
+
+                isCrouching = true;
+            }
+        }
     }
 
     private void ApplyGravity()
